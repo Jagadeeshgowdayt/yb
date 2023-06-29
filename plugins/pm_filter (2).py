93fdf76a69082919bd53e8d22842a7ce8066d755
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-06-29 11:35:32 +0530

    Update pm_filter (2).py

--- plugins/pm_filter (2).py
+++ plugins/pm_filter (2).py
@@ -1467,7 +1467,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.edit_message_media(
             query.message.chat.id, 
             query.message.id, 
-            InputMediaPhoto(random.choice(SPELL_IMG))
+            InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
             text=script.RAJKUMAR,
