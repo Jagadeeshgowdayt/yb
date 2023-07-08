875bb46f4c6d125d7009514419e3ee256b880977
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-08 16:08:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -61,6 +61,19 @@ async def how_to_use(client, message):
             parse_mode=enums.ParseMode.HTML
         )
 
+@Client.on_message(filters.command("how_to_use") & filters.incoming)
+async def how_to_use(client, message):
+        buttons = [[
+                    InlineKeyboardButton(text="❌️𝖢𝗅𝗈𝗌𝖾❌️", callback_data="close_data")
+                  ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.HOW.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML
+        )
+
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
