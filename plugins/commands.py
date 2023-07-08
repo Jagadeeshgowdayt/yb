561be7b1cfb77b8f8e909655aca0ba0278fe2ff9
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-08 15:54:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -50,19 +50,6 @@ async def movies(client, message):
             parse_mode=enums.ParseMode.HTML
         )
 
-@Client.on_message(filters.command("wwr") & filters.incoming)
-async def serials(client, message):
-    if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        buttons = [[
-                    InlineKeyboardButton(text="𝖢𝗅𝗈𝗌𝖾", callback_data="close_data")
-                  ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.WWR.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-
 @Client.on_message(filters.command("how_to_use") & filters.incoming)
 async def how_to_use(client, message):
         buttons = [[
