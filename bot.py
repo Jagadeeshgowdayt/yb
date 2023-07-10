f1e8b076ea55899853edcf15ce8b8c39ba8c34ca
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-10 19:25:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,6 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            bot_token=BOT_TOKEN1,
             workers=200,
             plugins={"root": "plugins"},
             sleep_threshold=10,
