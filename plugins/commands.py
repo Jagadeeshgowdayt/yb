915d4ca0f8a84e70e3ece2ac88362d65b697280c
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-11 23:12:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,20 +18,6 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command("new_movies") & filters.incoming)
-async def new_movies(client, message):
-        buttons = [[
-                    InlineKeyboardButton('Iɴʟɪɴᴇ Sᴇᴀʀᴄʜ ☌', switch_inline_query_current_chat=james),
-                    InlineKeyboardButton(text="𝖢𝗅𝗈𝗌𝖾", callback_data="close_data")
-                  ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.RECENT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-
 @Client.on_message(filters.command("serial") & filters.incoming)
 async def serial(client, message):
         buttons = [[
