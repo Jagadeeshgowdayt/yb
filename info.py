a4bb5b9c72fbe0b69b2541f6b2f4d3cdc83a974b
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-12 20:01:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), True)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/how_to_use_bot_rvasp/15")
 VERIFY2_URL = environ.get('VERIFY2_URL', "kpslink.in")
 VERIFY2_API = environ.get('VERIFY2_API', "722dfee670e99b2b8476a7beffdbe793d118d012")
