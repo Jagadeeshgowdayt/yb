f65de1b6381f5a9662ff7cb20e6f67b52f33eae2
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2023-07-13 15:31:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2903,6 +2903,21 @@ async def auto_filter(client, msg, spoll=False):
     if spoll:
         await msg.message.delete()
 
+async def auto_filter(client, msg, spoll=False):
+    reqstr1 = msg.from_user.id if msg.from_user else 0
+    reqstr = await client.get_users(reqstr1)
+    if not spoll:
+    m = await message.reply_sticker("CAACAgUAAxkBAAFDeptkr5TtVyMkIs4TevoXZWVBRss-6wACDAoAAjC_eFWxw_Gah5zk_R4E")
+    
+    # Delay for 2 seconds
+    await asyncio.sleep(2)
+    
+    # Delete the sticker
+    await m.delete()
+
+    rk = await message.reply_sticker("CAACAgUAAxkBAAFDeLdkr4COi51mvyZUAAG5l0EmIiwY2J4AAswKAALopYBVbzDUtgk8NrIeBA")
+
+
 
 async def advantage_spell_chok(client, msg):
     mv_id = msg.id
